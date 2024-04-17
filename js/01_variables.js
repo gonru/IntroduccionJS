367f7c4185b8766813bff262d22ada675c0fd868
Gonzalo Ruiz <gonzalo.ruiz.anabalon@gmail.com>
2024-04-16 22:58:12 -0400

    Seccion 10 clase 105 - variables con var

---
+++ js/01_variables.js
@@ -0,0 +1,33 @@
+// comentarios
+// una línea
+/*
+	multiples líneas
+*/
+
+// Variables
+
+// no se utiliza var
+
+var producto = "Audifonos Gamer";
+
+var disponible; // iniciamos la variable sin valor
+
+producto = true; // Reasignando el valor de la variables
+
+disponible = true;
+
+var producto1 = "Computadora",
+  disponible = true,
+  categoria = "Computadoras";
+
+// Reglas como no deben nombrar las variables
+// var 1disponible;
+// var -disponible;
+
+// esitlos de nombrar variables
+var nombre_producto = "Monitor HD"; // Underscore
+var nombreProducto = "Monitor HD"; // Camelcase
+var NombreProducto = "Monitor HD"; // Pascal Case (usada en clases en JS)
+var nombreproducto = "Monitor HD"; 
+
+console.log(producto);
