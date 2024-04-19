798a11b0efefbd2de2744baf2b6401f790de8746
Gonzalo Ruiz <gonzalo.ruiz.anabalon@gmail.com>
2024-04-18 23:12:11 -0400

    seccion 10 - clase 111 booleans

---
+++ js/09_booleanos.js
@@ -0,0 +1,11 @@
+// Booleanos
+
+const boolean1 = true;
+const boolean2 = false;
+
+console.log(boolean1);
+console.log(boolean2);
+
+// inicializar como objeto
+const boolean3 = new Boolean(true);
+console.log(typeof boolean3);
