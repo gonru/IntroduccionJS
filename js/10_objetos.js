26ebdc6c737eaa4bc7d99da6d8685f9a6d524b16
Gonzalo Ruiz <gonzalo.ruiz.anabalon@gmail.com>
2024-04-18 23:17:35 -0400

    seccion 10 - clase 113 crear objetos en js

---
+++ js/10_objetos.js
@@ -0,0 +1,22 @@
+// Objetos
+
+// const nombreProducto = "Monitor 20 Pulgadas";
+// const precio = 300;
+// const disponible = true;
+
+// este el objeto creado
+const producto = {
+  nombreProducto: "Monitor 20 Pulgadas",
+  precio: 300,
+  disponible: true,
+};
+
+console.log(producto);
+console.log(producto.nombreProducto);
+console.log(producto.precio);
+console.log(producto.disponible);
+
+// otra forma acceder al objeto
+console.log(producto["nombreProducto"]);
+console.log(producto["precio"]);
+console.log(producto["disponible"]);
